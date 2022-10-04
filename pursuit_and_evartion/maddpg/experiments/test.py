1ab5a2de3ab5ee114393511efddf50769043e5e2
rRmaru <MruD24@outlook.com>
2022-10-04 05:18:53 +0000

    DISPLAY変数を設定

--- pursuit_and_evartion/maddpg/experiments/test.py
+++ pursuit_and_evartion/maddpg/experiments/test.py
@@ -1,1 +1,14 @@
-print("ai")
+#%%
+import gym
+env = gym.make("MountainCar-v0")
+
+# %%
+from gym import envs
+envids = [spec.id for spec in envs.registry.all()]
+
+observation = env.reset()
+
+# %%
+env.render()
+env.close()
+# %%
