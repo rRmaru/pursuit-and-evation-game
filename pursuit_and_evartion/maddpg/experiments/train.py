d22878eec64103bdd64fdb86f4a8a59d14d54ae8
rRmaru <MruD24@outlook.com>
2022-10-25 09:35:13 +0000

    10/25

--- pursuit_and_evartion/maddpg/experiments/train.py
+++ pursuit_and_evartion/maddpg/experiments/train.py
@@ -15,7 +15,7 @@ def parse_args():
     parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
     # Environment
     parser.add_argument("--scenario", type=str, default="pursuit_and_evation", help="name of the scenario script")
-    parser.add_argument("--max-episode-len", type=int, default=10000, help="maximum episode length")
+    parser.add_argument("--max-episode-len", type=int, default=200, help="maximum episode length")
     parser.add_argument("--num-episodes", type=int, default=60000, help="number of episodes")
     parser.add_argument("--num-adversaries", type=int, default=0, help="number of adversaries")
     parser.add_argument("--good-policy", type=str, default="maddpg", help="policy for good agents")
